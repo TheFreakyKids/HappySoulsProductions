e0c051f077e01336df3de8d8e459d41ec3828b69
Robert <robby.patterson95@yahoo.com>
2018-04-19 14:56:46 -0700

    removing dummy blocks and ui
    discovered david bowie knife wont be as meme worthy ;-;

---
+++ HighNoon/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public bool isPlayer1;
+    public bool isPlayer2;
+    
+	void Start ()
+    {
+        if(this.gameObject.tag == "Player1")
+        {
+            isPlayer1 = true;
+        }
+        else
+        {
+            isPlayer2 = true;
+        }
+    }
+	
+	void Update ()
+    {
+        #region Player1
+        if(isPlayer1 == true)
+        {
+
+        }
+        #endregion
+
+        #region Player2
+        else if (isPlayer2 == true)
+        {
+
+        }
+        #endregion 
+    }
+}
