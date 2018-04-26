ef8d389bbd5a2b1fbb36bda4545bfcebea383532
Unknown <main.jesusramos@gmail.com>
2018-04-25 21:37:33 -0700

    Wed Work
    Player Spawning works, starting on GameManager, ohter stuff

---
+++ HighNoon/Assets/JesusStuff/Jesus' Work/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float spawnTime = 4f;
+
+    private void Update()
+    {
+        if ()
+        {
+
+        }
+        StartCoroutine(Respawn());
+    }
+
+    public IEnumerator Respawn()
+    {
+        yield return new WaitForSecondsRealtime(spawnTime);
+
+        if (//something)
+        {
+            #region Spawning
+            int point = Random.Range(0, 7);
+
+            switch (point)
+            {
+                case 0:
+                    transform.position = GameObject.Find("SpawnPoint0").transform.position;
+                    break;
+                case 1:
+                    transform.position = GameObject.Find("SpawnPoint1").transform.position;
+                    break;
+                case 2:
+                    transform.position = GameObject.Find("SpawnPoint2").transform.position;
+                    break;
+                case 3:
+                    transform.position = GameObject.Find("SpawnPoint3").transform.position;
+                    break;
+                case 4:
+                    transform.position = GameObject.Find("SpawnPoint4").transform.position;
+                    break;
+                case 5:
+                    transform.position = GameObject.Find("SpawnPoint5").transform.position;
+                    break;
+                case 6:
+                    transform.position = GameObject.Find("SpawnPoint6").transform.position;
+                    break;
+                case 7:
+                    transform.position = GameObject.Find("SpawnPoint7").transform.position;
+                    break;
+            }
+            #endregion
+            print("respawned");
+        }
+    }
+}
--- HighNoon/Assets/JesusStuff/Jesus' Work/Scripts/Player.cs
+++ HighNoon/Assets/JesusStuff/Jesus' Work/Scripts/Player.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,11 +9,6 @@ public class Player : MonoBehaviour
 
     public MonoBehaviour fpsCon;
 
-	public void Start ()
-    {
-		
-	}
-	
 	private void Update ()
     {
         Mathf.Clamp(health, 0, 100);
@@ -41,21 +35,51 @@ public class Player : MonoBehaviour
 
     public IEnumerator Respawn ()
     {
-        yield return new WaitForSeconds(spawnTime);
-
-        //move to spawn point
+        yield return new WaitForSecondsRealtime(spawnTime);
 
         if (health != 100f)
         {
-            print("respawned");
+            #region Spawning
+            int point = Random.Range(0, 7);
+
+            switch (point)
+            {
+                case 0:
+                    transform.position = GameObject.Find("SpawnPoint0").transform.position;
+                    break;
+                case 1:
+                    transform.position = GameObject.Find("SpawnPoint1").transform.position;
+                    break;
+                case 2:
+                    transform.position = GameObject.Find("SpawnPoint2").transform.position;
+                    break;
+                case 3:
+                    transform.position = GameObject.Find("SpawnPoint3").transform.position;
+                    break;
+                case 4:
+                    transform.position = GameObject.Find("SpawnPoint4").transform.position;
+                    break;
+                case 5:
+                    transform.position = GameObject.Find("SpawnPoint5").transform.position;
+                    break;
+                case 6:
+                    transform.position = GameObject.Find("SpawnPoint6").transform.position;
+                    break;
+                case 7:
+                    transform.position = GameObject.Find("SpawnPoint7").transform.position;
+                    break;
+            }
+            #endregion
+
             health = 100f;
             fpsCon.enabled = true;
+            print("respawned");
         }
     }
 
     private void Suicide ()
     {
-        print("kys");
         health = 0f;
+        print("kys");
     }
 }
